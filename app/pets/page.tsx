0161330ea49f5ee9fa01c532900b7c7dd495d95d
Alena <al.gnevusheva@gmail.com>
2024-05-03 12:00:36 +0200

    make dynamic routing for pets

--- app/pets/page.tsx
+++ app/pets/page.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link"
+
+const petsLinks = [
+  {
+    href: "/oxy",
+    label: "Oxy",
+  },
+  {
+    href: "/doxie",
+    label: "Doxie",
+  },
+]
+
 const PetsPage = () => {
-  return <div>Pets</div>
+  return (
+    <div>
+      {petsLinks.map((link) => (
+        <Link key={link.href} href={`/pets/${link.href}`}>
+          {link.label}
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 export default PetsPage
